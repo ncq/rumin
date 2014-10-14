6a09880977025e54f257f742cfa53f3f169d7f24
KairiTakemura <thinglikeabar@gmail.com>
2014-10-15 02:10:29 +0900

    store

--- src/caller.rb
+++ src/caller.rb
@@ -1,15 +1,3 @@
 class Caller
-  attr_accessor :str1, :str2
-
-  def initialize
-    @str1 = "baz"
-    @str2 = "bar"
-  end
-
-  def print
-    puts @str1
-    puts @str2
-  end
+  attr_accessor :body
 end
-
-
--- src/rumin.c
+++ src/rumin.c
@@ -7,40 +7,40 @@
 #include "mruby/string.h"
 
 int main() {
+    char buf[BUFFSIZE];
+    int input;
 
-/*    char buf[BUFFSIZE];
-
+    // curses初期化
     initscr();
-    cbreak();    // キー入力を直ちに受け付ける
+    cbreak();
+
+    // キーボード入力受け付け
     getstr(buf);
-    addstr(buf);
-    endwin();
-*/
 
-    mrb_state* mrb = mrb_open();
+    mrb_state *mrb = mrb_open();
     // mrubyファイルをロードする
-    //FILE *f = fopen("caller.mrb", "r");
-    FILE* f = fopen("caller.rb", "r");
-
-    //mrb_load_irep_file(mrb, f);
+    FILE *f = fopen("caller.rb", "r");
     mrb_load_file(mrb, f);
 
-    //fclose(f);
-printf("check\n");
     // クラスオブジェクトを取得する
-    struct RClass* caller = mrb_class_get(mrb, "Caller");
-printf("check\n");
+    struct RClass *caller = mrb_class_get(mrb, "Caller");
 
     // 引数をmrb_valueに変換する
     mrb_value caller_value = mrb_obj_value(caller);
-    mrb_value str1_value = mrb_str_new(mrb, "hoge", 4);
-    mrb_value str2_value = mrb_str_new(mrb, "fuga", 4);
+    mrb_value body_input = mrb_str_new(mrb, buf, strlen(buf));
 
     // Caller#newを呼び出す
-    mrb_value call = mrb_funcall(mrb, caller_value, "new", 2, str1_value, str2_value);
+    mrb_value call = mrb_funcall(mrb, caller_value, "new", 0);
 
-    // Caller#printを呼び出す
-    mrb_funcall(mrb, call, "print", 0);
+    // 入力値の格納（アクセサメソッドを実行）
+    mrb_funcall(mrb, call, "body=", 1, body_input);
+    // 入力値の取得（アクセサメソッドを実行）
+    mrb_value body_output = mrb_funcall(mrb, call, "body", 0);
+    const char *body = mrb_string_value_ptr(mrb, body_output);
+    addstr(body);
+    // 画面を表示するために入力待ちにする
+    //input = getch();
+    endwin();
 
     mrb_close(mrb);
 
