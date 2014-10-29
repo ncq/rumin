5485a22b7e0cfec753066f4a6af099271c69f196
kazuhide maeda <kazuhide@cupertino.local>
2014-10-30 02:02:57 +0900

    [wip] created editor.rb and list.rb

---
+++ mruby/editor.rb
@@ -0,0 +1,28 @@
+class Editor
+  @buffer_chain
+  @current_buffer
+
+  def initialize
+  end
+
+  def finish
+  end
+
+  def save
+  end
+
+  def load
+  end
+
+  def create_buffer
+  end
+
+  def clear_buffer
+  end
+
+  def delete_buffer
+  end
+
+  def set_current_buffer
+  end
+end
---
+++ mruby/list.rb
@@ -0,0 +1,11 @@
+class LinkedList
+  @cells
+
+  def initialize
+  end
+  
+end
+
+class Cell
+  attr_accessor :prev, :content, :next
+end
