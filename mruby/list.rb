36a175c238dd15f5e7e0cd61e6330b6f8e72dd0f
kazuhide maeda <kazuhide@cupertino.local>
2014-11-05 01:50:30 +0900

    wip:list.rb

--- mruby/list.rb
+++ mruby/list.rb
@@ -1,11 +1,85 @@
-class LinkedList
-  @cells
+class InteractiveLinkedList
+  class Item
+    attr_accessor :prev, :content, :next
+
+    def initialize(content = nil)
+      @content = content
+    end
+  end
+
+  class ItemIterator
+    include Enumerable
+
+    def initialize(head)
+      @head = head
+    end
+
+    def each
+      item = @head
+      while not item.next == @head
+        item = item.next
+        yield item
+      end
+      self
+    end
+  end
+
+  class ItemNotFoundException < Exception; end
+
+  include Enumerable
+
+  attr_reader :head
 
   def initialize
+    @head = Item.new
+    @head.next = @head.prev = @head
   end
-  
-end
 
-class Cell
-  attr_accessor :prev, :content, :next
+  def each
+    items.each {|item| yield item.data}
+    self
+  end
+
+  def push(content)
+    new_item = Item.new(content)
+    tail = find_tail_item
+    new_item.next = head
+    new_item.prev = tail
+    tail.next = new_item
+    head.prev = newitem
+    self
+  end
+
+  def delete_at(n)
+    item = find_item(n)
+    item.prev.next = item.next
+    item.next.prev = item.prev
+    self
+  end
+
+  def []=(key,value)
+    find_item(key).content = value
+  end
+
+  def [](key)
+    find_item(key).data
+  end
+
+  private
+  def items
+    ItemIterator.new(head)
+  end
+
+  def find_tail_item
+    items.reduce(head) {|last_item, next_item| next_item}
+  end
+
+  def find_item(n)
+    item = head
+    (n + 1).times do
+      raise ItemNotFoundException if item.next == head
+      item = item.next
+    end
+    items
+  end
 end
