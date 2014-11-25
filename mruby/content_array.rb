90e81e24a1635d8eaaeb1785612cfefdc1e1cdef
k-hibi <k_hibi@e-savaway.jp>
2014-11-25 21:59:58 +0900

    fix cursor move bug

--- mruby/content_array.rb
+++ mruby/content_array.rb
@@ -119,8 +119,16 @@ class ContentArray < Content
     len  = line.length
     return 0 if len == 0
     converter = create_point_cursor_converter(row, len, true)
-    return (converter[len - 1] + 1) if col > converter[len - 1]
+    # TODO:最後の文字が半角のとき1文字、全角のときに2文字プラスするようにする
+    if col > converter[len - 1]
+      if Utf8Util::full_width?(line[-1])
+        return (converter[len - 1] + 2)
+      else
+        return (converter[len - 1] + 1)
+      end
+    end
     return converter.include?(col) ? col : col - 1
+    col
   end
 
   private
