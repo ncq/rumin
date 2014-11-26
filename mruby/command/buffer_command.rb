182559f742413c621bfd7502500d66bbe3e577ab
KairiTakemura <thinglikeabar@gmail.com>
2014-11-26 17:37:53 +0900

    bug fix

--- mruby/command/buffer_command.rb
+++ mruby/command/buffer_command.rb
@@ -4,12 +4,15 @@ class BufferCommand
     def up(buffer)
       buffer.move_line(-1)
     end
+
     def down(buffer)
       buffer.move_line(1)
     end
+
     def left(buffer)
       buffer.move_point(-1)
     end
+
     def right(buffer)
       buffer.move_point(1)
     end
@@ -17,11 +20,17 @@ class BufferCommand
     def change_line(buffer)
       buffer.change_line
     end
+
     def buffer_delete(buffer)
       buffer.delete(-1)
     end
+
     def buffer_del2(buffer)
       buffer.delete(-2)
+    end
+
+    def eval_current_buffer(buffer)
+      eval(buffer.content.to_string)
     end
 
     def set_copy_mark(buffer)
@@ -35,6 +44,5 @@ class BufferCommand
     def paste_string(buffer)
       buffer.paste_string
     end
-
   end
 end
