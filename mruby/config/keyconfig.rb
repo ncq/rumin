b125abbca0067b279d4f1f5a40dd7b52cc4805e5
a1125ts <shibata@shibata-no-MacBook-Pro.local>
2015-01-25 21:10:20 +0900

    use emacs like keybind

--- mruby/config/keyconfig.rb
+++ mruby/config/keyconfig.rb
@@ -1,74 +1,4 @@
-# bind 'up' do |b|
-#   b.move_line -1
-# end
-# 
-# bind 'down' do |b|
-#   b.move_line 1
-# end
-# 
-# bind 'left' do |b|
-#   b.move_point -1
-# end
-# 
-# bind 'right' do |b|
-#   b.move_point 1
-# end
-# 
-# bind 'ctrl-p' do |b|
-#   b.move_line -1
-# end
-# 
-# bind 'ctrl-n' do |b|
-#   b.move_line 1
-# end
-# 
-# bind 'ctrl-f' do |b|
-#   b.move_point 1
-# end
-# 
-# bind 'ctrl-b' do |b|
-#   b.move_point -1
-# end
-# 
-# bind 'ctrl-j' do |b|
-#   b.change_line
-# end
-# 
-# bind 'ctrl-e' do |b|
-#   b.eval_content
-# end
-# 
-# bind 'ctrl-r' do |b|
-#   b.insert_evaluated_content_comment
-# end
-# 
-# bind 'ctrl-l' do |b|
-#   b.insert_evaluated_line_comment
-# end
-# 
-# bind 'ctrl-t' do |b|
-#   b.set_evaluate_mark # markセット
-# end
-# 
-# bind 'ctrl-i' do |b|
-#   b.insert_evaluated_region_comment # 実行
-# end
-# 
-# bind 'ctrl-[' do |b|
-#   b.display.echo.print_message(b.display.echo.get_parameter("input string:"))
-# end
-# 
-# bind 'ctrl-o' do |b|
-#   b.open_file
-# end
-# 
-# bind 'ctrl-p' do |b|
-#   b.save_file
-# end
-# 
-# bind 'ctrl-d' do |b|
-#   b.delete(-1)
-# end
+# coding: utf-8
 
 bind %w(up) do |b|
 	b.move_line -1
@@ -86,18 +16,63 @@ bind %w(right) do |b|
 	b.move_point 1
 end
 
-bind %w(ctrl-x ctrl-p) do |b|
+bind %w(ctrl-p) do |b|
 	b.move_line -1
 end
 
-bind %w(ctrl-x ctrl-n) do |b|
+bind %w(ctrl-n) do |b|
 	b.move_line 1
 end
 
-bind %w(ctrl-x ctrl-b) do |b|
+bind %w(ctrl-b) do |b|
 	b.move_point -1
 end
 
-bind %w(ctrl-x ctrl-f) do |b|
+bind %w(ctrl-f) do |b|
 	b.move_point 1
+end
+
+bind %w(ctrl-j) do |b|
+  b.change_line
+end
+
+bind %w(ctrl-m) do |b|
+  b.change_line
+end
+
+bind %w(ctrl-sp) do |b|
+  b.set_evaluate_mark # markセット
+end
+
+bind %w(ctrl-x ctrl-i) do |b|
+  b.insert_evaluated_region_comment # 実行
+end
+
+bind %w(ctrl-[) do |b|
+  b.display.echo.print_message(b.display.echo.get_parameter("input string:"))
+end
+
+bind %w(ctrl-x ctrl-f) do |b|
+  b.open_file
+end
+
+bind %w(ctrl-x ctrl-s) do |b|
+  b.save_file
+end
+
+bind %w(ctrl-d) do |b|
+  b.move_point 1
+  b.delete(-1)
+end
+
+bind %w(ctrl-x ctrl-e) do |b|
+  b.eval_content
+end
+
+bind %w(ctrl-x ctrl-r) do |b|
+  b.insert_evaluated_content_comment
+end
+
+bind %w(ctrl-x ctrl-l) do |b|
+  b.insert_evaluated_line_comment
 end
