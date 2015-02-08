19a8cba3022134cc74320ce511fc033feb2c2ddd
kazuhide <kzhd15med3pub@gmail.com>
2015-02-08 06:38:41 +0000

    fix comments.

--- install-to-ubuntu.sh
+++ install-to-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PACKAGES="ruby ruby-dev g++ bison libyaml-dev libncurses5-dev libncursesw5-dev"
 GEMS="rake yard"
-echo "This install script will be installd following packages and gems."
+echo "This installation script will install the following packages and gems."
 echo "packages: ${PACKAGES}"
 echo "gems : ${GEMS}"
 echo "Are you sure?(yes/no)"
